92d2c61f33498e29a2d8a4ab93c34019dae46c84
mariushide <mariushi@hiof.no>
2025-10-24 18:50:25 +0200

    Oppdatert innloggingssystem

--- backend/src/main/java/hiof_project/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/hiof_project/infrastructure/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package hiof_project.infrastructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,7 +21,8 @@ public class SecurityConfig {
     // Konfigurerer hvilke URL-er som skal autentiseres.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth>/**").permitAll()
                         .anyRequest().authenticated());
