f149249dae9157296029f6677472d44165380302
Rafay2000 <rafayakbani@gmail.com>
2025-11-20 23:02:54 +0100

    Added comment

--- backend/src/main/java/hiof_project/infrastructure/config/RestTemplateConfig.java
+++ backend/src/main/java/hiof_project/infrastructure/config/RestTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+//Rest template for klasser som bruker det
 @Configuration
 public class RestTemplateConfig {
 
