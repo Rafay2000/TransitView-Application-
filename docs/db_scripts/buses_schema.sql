7c1640adb145d414b9ce9c5a14cf77e7076fa483
Rafay2000 <rafayakbani@gmail.com>
2025-11-20 19:09:51 +0100

    Added SQL Schema files for documentation

---
+++ docs/db_scripts/buses_schema.sql
@@ -0,0 +1,11 @@
+------------------------------------------------------------
+-- Busser (5 stk for testing)
+------------------------------------------------------------
+
+INSERT INTO Buses (bus_id, bus_name, bus_type, capacity) VALUES
+(1, 'Halden-101', 'Bybuss - Elektrisk', 55),
+(2, 'Halden-202', 'Bybuss - Bensin', 60),
+(3, 'Halden-303', 'Bybuss - Elektrisk', 50),
+(4, 'Halden-404', 'Mini-buss - Elektrisk', 25),
+(5, 'Halden-505', 'Turistbuss - Hybrid', 45);
+
