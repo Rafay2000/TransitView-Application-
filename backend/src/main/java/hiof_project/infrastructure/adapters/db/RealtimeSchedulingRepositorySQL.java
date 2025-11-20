6e52905677ed470ee2e1bf591a3dd7d2ed9a1f8d
mariushide <mariushi@hiof.no>
2025-11-20 23:23:59 +0100

    Ferdig med innloggingssystem, siste commit

--- backend/src/main/java/hiof_project/infrastructure/adapters/db/RealtimeSchedulingRepositorySQL.java
+++ backend/src/main/java/hiof_project/infrastructure/adapters/db/RealtimeSchedulingRepositorySQL.java
@@ -1,5 +1,5 @@
 package hiof_project.infrastructure.adapters.db;
-
+/*
 import hiof_project.domain.exception.RepositoryException;
 import hiof_project.domain.model.transport_system.RealtimeScheduling;
 import hiof_project.ports.out.RealtimeSchedulingRepository;
@@ -234,3 +234,4 @@ public class RealtimeSchedulingRepositorySQL implements RealtimeSchedulingReposi
     }
 }
 
+*/
